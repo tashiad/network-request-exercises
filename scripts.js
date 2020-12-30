52fdad2f492d14c1c62a3a9980cbdcf09f9a498a
tashiad <tashiadavis10@gmail.com>
2020-12-30 15:26:58 -0700

    Finalize delete request

--- scripts.js
+++ scripts.js
@@ -1,15 +1,12 @@
 const displayArea = document.querySelector(".display-area");
-const createButton = document.querySelector(".create-button");
-const deleteButton = document.querySelector(".delete-button");
+const addButton = document.querySelector(".add-button");
 const nameInput = document.querySelector(".animal-name-input");
 const dietInput = document.querySelector(".animal-diet-input");
 const funFactInput = document.querySelector(".animal-fun-fact-input");
 
 window.addEventListener('load', retrieveData);
-createButton.addEventListener('click', createAnimal);
-// deleteButton.addEventListener('click', () => {
-//   deleteAnimal()
-// });
+addButton.addEventListener('click', addAnimal);
+displayArea.addEventListener('click', deleteAnimal);
 
 function retrieveData() {
   fetch("http://localhost:3001/api/v1/animals")
@@ -21,16 +18,17 @@ function retrieveData() {
 function showAnimals(data) {
   data.forEach(animal => {
     displayArea.innerHTML += `
-    <article id=${animal.id}>
+    <article>
       <h2>${animal.name.charAt(0).toUpperCase() + animal.name.slice(1)}</h2>
       <p><strong>Diet: </strong>${animal.diet}</p>
       <p><strong>Fun Fact: </strong>${animal.fun_fact}</p>
+      <button class="delete-button" id=${animal.id}>Delete Animal</button>
     </article>
   `
   })
 }
 
-function createAnimal() {
+function addAnimal() {
   fetch("http://localhost:3001/api/v1/animals", {
       method: 'POST',
       headers: {
@@ -44,7 +42,7 @@ function createAnimal() {
       })
     })
     .then(response => response.json())
-    .then(data => retrieveData())
+    .then(data => showAnimals(data))
     .catch(error => console.log(error));
   clearInputs();
 }
@@ -55,104 +53,17 @@ function clearInputs() {
   funFactInput.value = "";
 }
 
-// function deleteAnimal() {
-//   fetch(`http://localhost:3001/api/v1/animals/${deleteAnimalByID.value}`, {
-//       method: 'DELETE',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     })
-//     .then(response => response.json())
-//     .then(data => retrieveData())
-//     .catch(error => console.log(error));
-//   clearInputs();
-// }
-
-
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// const displayUsers = document.querySelector("#display-users");
-// const displayAnimals = document.querySelector("#display-animals");
-// const animalButton = document.querySelector(".animal-button")
-//
-// animalButton.addEventListener('click', () => {
-//   deleteAnimal(3)
-// })
-//
-// function showUsers(users) {
-//   users.forEach(user => {
-//     displayUsers.innerText += `
-//     ${user.name} is ${user.status} and interested in: ${user.interests}
-//   `
-//   })
-// }
-//
-// fetch("http://localhost:3001/api/v1/users")
-//   .then(response => response.json())
-//   .then(data => showUsers(data))
-//   .catch(error => console.log(error));
-//
-// function showAnimals(animal) {
-//     displayAnimals.innerHTML = `
-//     <p id=${animal.id}>${animal.name} eats ${animal.diet} and ${animal.fun_fact}</p>
-//   `
-// }
-//
-// const options = {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     id: 5,
-//     name: 'moose',
-//     diet: 'no idea',
-//     fun_fact: 'they have antlers'
-//   }),
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// };
-//
-// function addAnimal() {
-//   fetch("http://localhost:3001/api/v1/animals", options)
-//     .then(response => response.json())
-//     .then(data => showAnimals(data))
-//     .catch(error => console.log(error));
-// }
-//
-// addAnimal()
-//
-// function deleteAnimal(id) {
-//   fetch(`http://localhost:3001/api/v1/animals/${id}`, {
-//     method: 'DELETE',
-//     headers: {
-//           'Content-Type': 'application/json'
-//         }
-//   })
-//     .then(response => response.json())
-//     .then(id => deleteHTML(id))
-//     .catch(error => console.log(error));
-// }
-//
-// function deleteHTML(id) {
-//   document.getElementById(id).remove();
-// }
-
-// function addAnimal(name, diet, funFact) {
-//   fetch("http://localhost:3001/api/v1/animals", {
-//     method: 'POST',
-//     body: JSON.stringify({
-//       id: Date.now(),
-//       name: name,
-//       diet: diet,
-//       fun_fact: funFact
-//     }),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//     .then(response => response.json())
-//     .then(data => showAnimals(data))
-//     .catch(error => console.log(error));
-//
-//   // showAnimals(animals)
-// }
+function deleteAnimal(event) {
+  fetch(`http://localhost:3001/api/v1/animals/${event.target.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      displayArea.innerHTML = "";
+      showAnimals(data)
+    })
+    .catch(error => console.log(error));
+}
